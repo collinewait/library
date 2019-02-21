a173ef2e7426c35e83506b8b19fbf32f723901fe
collinewait <collinewait17@gmail.com>
2019-02-21 18:52:52 +0300

    render pages

--- app.js
+++ app.js
@@ -19,10 +19,39 @@ app.use('/js',
   express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
+const books = [
+  {
+    title: 'War and peace',
+    genre: 'Historical fiction',
+    author: 'Tolstoy',
+    read: false,
+  },
+  {
+    title: 'Less miserables',
+    genre: 'Historical fiction',
+    author: 'Victor Hugo',
+    read: false,
+  },
+  {
+    title: 'The time Machine',
+    genre: 'Science fiction',
+    author: 'H. G. Wells',
+    read: false,
+  },
+];
 bookRouter.route('/')
   .get((req, res) => {
-    res.send('Hello books');
+    res.render(
+      'books',
+      {
+        title: 'Library',
+        nav: [
+          { link: '/books', title: 'Books' },
+          { link: 'authors', title: 'Authors' },
+        ],
+        books,
+      },
+    );
   });
 bookRouter.route('/single')
   .get((req, res) => {
