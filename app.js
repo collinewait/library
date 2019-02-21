0766f51018140a68bc8c9895f4f64764fe7be15c
collinewait <collinewait17@gmail.com>
2019-02-21 11:44:25 +0300

    serve index

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 app.use(morgan('tiny'));
 app.get('/', (req, res) => {
-  res.send('Hello from my library app');
+  res.sendFile(path.join(__dirname, 'views/index.html'))
 })
 
 app.listen(3000, () => {
