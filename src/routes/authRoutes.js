7a1818ed45b2088ad7d5bcf65fc316c6cc03703f
collinewait <collinewait17@gmail.com>
2019-02-23 15:56:27 +0300

    signin user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
+const passport = require('passport');
 
 const authRouter = express.Router();
 
-function router() {
+function router(nav) {
   authRouter.route('/signup')
     .post((req, res) => {
       const { username, password } = req.body;
@@ -28,8 +29,18 @@ function router() {
           debug(err);
         }
       }());
-
     });
+  authRouter.route('/signin')
+    .get((req, res) => {
+      res.render('signin', {
+        nav,
+        title: 'Sign In',
+      });
+    })
+    .post(passport.authenticate('local', {
+      successRedirect: '/auth/profile',
+      failureRedirect: '/',
+    }));
   authRouter.route('/profile')
     .get((req, res) => {
       res.json(req.user);
