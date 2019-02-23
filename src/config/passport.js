6e2b2557236e2dce6421d94b76ddd88927359a4b
collinewait <collinewait17@gmail.com>
2019-02-23 15:40:38 +0300

    create signup with passport-local

---
+++ src/config/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+require('./strategies/local.strategy.js');
+
+module.exports = function passportConfig(app) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+  // stores user in a session
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  // Retrieves user from a session
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+
+};
